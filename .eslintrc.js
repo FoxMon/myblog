a7bc651fa5df00f37132363299e3de54145d078a
foxmon <jjockrod@naver.com>
2022-05-31 00:27:26 +0900

    eslint수정

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,6 +21,22 @@ module.exports = {
         "vue/attribute-hyphenation": ["error", "never", {
             "ignore": []
         }],
+        "vue/html-indent": ["error", 4, {
+            "attribute": 1,
+            "baseIndent": 1,
+            "closeBracket": 0,
+            "alignAttributesVertically": true,
+            "ignores": []
+        }],
+        "vue/html-self-closing": ["error", {
+            "html": {
+                "void": "always",
+                "normal": "never",
+                "component": "always"
+            },
+            "svg": "always",
+            "math": "always"
+        }],
     },
     settings: {
         "import/resolver": {
